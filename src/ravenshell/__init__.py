bf758267e920eb74625ae8b387fb23ad305ef6f0
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-05-06 12:04:54 +0200

    Traceback support.

--- src/ravenshell/__init__.py
+++ src/ravenshell/__init__.py
@@ -1,19 +1,57 @@
 from __future__ import print_function
 
+from collections import namedtuple
 import ConfigParser
 import argparse
 import logging
 import os
 import sys
-from raven.utils.stacks import get_lines_from_file
 
 from raven import Client
 from raven.conf import setup_logging
 from raven.handlers.logging import SentryHandler
+from raven.utils.stacks import get_lines_from_file
 
 SYSTEM_CONF = '/etc/raven.conf'
 USER_CONF = os.path.expanduser('~/.raven.conf')
 
+class BashLog(Exception):
+    pass
+
+class Code(object):
+    def __init__(self, co_filename, co_name):
+        self.co_filename = co_filename
+        self.co_name = co_name
+
+
+class Frame(object):
+    def __init__(self, f_code, module=None):
+        self.f_code = f_code
+        self.f_globals = {'__name__': module}
+
+
+class Traceback(object):
+    def __init__(self, tb_frame, tb_lineno, tb_next):
+        self.tb_frame = tb_frame
+        self.tb_lineno = tb_lineno
+        self.tb_next = tb_next
+
+
+def get_traceback(lines_from_file=3):
+    tb = os.environ.get('TRACEBACK')
+    last_tb = None
+    if tb is not None:
+        files, lines, funcs = tb.split(';')
+        for file, line, funcname in zip(files.split(','),
+                                        lines.split(','),
+                                        funcs.split(',')):
+            file = os.path.realpath(file)
+            current_tb = Traceback(
+                tb_frame=Frame(Code(file, funcname), module=file),
+                tb_lineno=int(line), tb_next=last_tb)
+            last_tb = current_tb
+
+    return last_tb
 
 def abort(message, retval=1):
     print(message)
@@ -29,14 +67,13 @@ def raise_():
 
     levels = parser.add_subparsers(dest="level", help="Log level.")
 
-    exception = levels.add_parser("exception")
     critical = levels.add_parser("critical")
     error = levels.add_parser("error")
     warning = levels.add_parser("warning")
     info = levels.add_parser("info")
     debug = levels.add_parser("debug")
 
-    for level in (exception, critical, error, warning, info, debug):
+    for level in (critical, error, warning, info, debug):
         level.add_argument("project", nargs='?', action="store",
                            default=os.environ.get('SENTRY_PROJECT'))
         level.add_argument("message", nargs=1, action="store")
@@ -89,10 +126,7 @@ def raise_():
     setup_logging(handler)
     logger = logging.getLogger(args.logger)
 
-    source = os.environ.get('BASH_SOURCE')
-    lineno = os.environ.get('LINENO')
-    print(os.environ)
-    print(get_lines_from_file(source, lineno, 3))
-
-    getattr(logger, args.level)(args.message[0], exc_info=False,
-            extra=os.environ)
+    traceback = get_traceback()
+    getattr(logger, args.level)(args.message[0],
+            extra=os.environ,
+            exc_info=(BashLog, args.message[0], traceback))
